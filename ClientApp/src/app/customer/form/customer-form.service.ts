af7473cc33513ce55531e37ba5574b7cf3e8e16c
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2020-11-19 11:52:53 +0100

    prepare date

--- ClientApp/src/app/customer/form/customer-form.service.ts
+++ ClientApp/src/app/customer/form/customer-form.service.ts
@@ -41,7 +41,7 @@ export class CustomerFormService {
     return this.formBuilder.group({
       id: [initialData.id],
       name: [initialData.name, [Validators.required]],
-      birthday: [initialData.birthday, []],
+      birthday: [(initialData.birthday || '').split('T')[0], []],
       cpf: [initialData.cpf, [this.validatorService.cpf()]],
       rg: [initialData.rg, []],
       sites: this.formBuilder.array(
